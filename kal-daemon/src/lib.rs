26fb150165d23bfab70903d56932ccc5f2e5b03c
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-01-04 02:14:45 +0100

    daemon: clean

--- kal-daemon/src/lib.rs
+++ kal-daemon/src/lib.rs
@@ -1,8 +1,10 @@
+mod daemon;
 mod mode;
 mod schedule;
 mod temperature;
 mod time;
 
+pub use crate::daemon::Daemon;
 pub use crate::mode::Mode;
 pub use crate::schedule::Schedule;
 pub use crate::temperature::Temperature;
--- kal-daemon/src/main.rs
+++ kal-daemon/src/main.rs
@@ -1,67 +1,10 @@
-use kal_daemon::{Mode, Schedule, Time};
-use log::{debug, info};
+use kal_daemon::Daemon;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let mut mode = Mode::default();
-    let schedule = Schedule::default();
-    let mut config = zenoh::Config::default();
-    config
-        .insert_json5("connect/endpoints", "[\"tcp/127.0.0.1:7447\"]")
-        .unwrap();
-    let session = zenoh::open(config).await.unwrap();
-    let replies = session.get("kal/cmnd/daemon/mode").await.unwrap();
-    while let Ok(reply) = replies.recv_async().await {
-        if let Ok(payload) = reply.result().unwrap().payload().try_to_string() {
-            mode = payload.as_ref().into();
-            info!("mode {mode}");
-        }
-    }
-    let mode_subscriber = session
-        .declare_subscriber("kal/cmnd/daemon/mode")
-        .await
-        .unwrap();
-    let temperature_subscriber = session
-        .declare_subscriber("kal/tele/tasmota_43D8FD/temperature")
-        .await
-        .unwrap();
-
+    let mut daemon = Daemon::init().await;
     loop {
-        tokio::select! {
-            reply = mode_subscriber.recv_async() => {
-                if let Ok(sample) = reply {
-                    if let Ok(payload) = sample.payload().try_to_string() {
-                        mode = payload.as_ref().into();
-                        info!("mode {mode}");
-                        if mode == Mode::On {
-                            debug!("-> relay On");
-                            session.put("kal/cmnd/garage/relay", "On").await.unwrap();
-                        } else if mode == Mode::Off {
-                            debug!("-> relay Off");
-                            session.put("kal/cmnd/garage/relay", "Off").await.unwrap();
-                        }
-                    }
-                }
-            }
-            reply = temperature_subscriber.recv_async() => {
-                if let Ok(sample) = reply {
-                    if let Ok(payload) = sample.payload().try_to_string() {
-                        if let Ok(v) = payload.parse::<f64>() {
-                            let t = v.into();
-                            debug!("received {t}");
-                            let h = match mode {
-                                Mode::Auto => schedule.auto(Time::now(), t),
-                                Mode::On => true,
-                                Mode::Off => false,
-                            };
-                            let p = if h { "On" } else { "Off" };
-                            debug!("relay {p}");
-                            session.put("kal/cmnd/garage/relay", p).await.unwrap();
-                        };
-                    };
-                }
-            }
-        };
+        daemon.select().await;
     }
 }
